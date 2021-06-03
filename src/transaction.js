786786ca7d0ca7448e86e1e4fb2e1122f1412663
Toby Jessup <toby4444@hotmail.com>
2021-06-03 16:12:32 +0100

    test for get amount added, passing both date and amount tests

--- src/transaction.js
+++ src/transaction.js
@@ -2,4 +2,18 @@
 
 class Transaction{
 
-}
+
+    constructor(date = new getDate().toLocaleDateString(), amount){
+        this.date = date
+        this.amount = amount
+    };
+
+    getDate(){
+        console.log(this.date);
+        return this.date;
+    };
+
+    getAmount(){
+        return this.amount;
+    };
+};
