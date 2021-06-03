5134df3fa3c11008647ff366b96dd283fea2f6cc
Toby Jessup <toby4444@hotmail.com>
2021-06-03 16:16:32 +0100

    added test for getting balance with transaction

--- spec/transactionSpec.js
+++ spec/transactionSpec.js
@@ -1,18 +1,36 @@
 'use strict'
 
 describe("Transcation", function(){
+    let depositTransaction;
+    let withdrawTransaction;
+    // var withdraw = -50
+    // deposit = 50
 
-    let transaction;
-    var date = getDate()
-    var deposit = 1000
-    var withdrawal = 500
+    beforeEach(function(){
+        depositTransaction = new Transaction("18/02/2020", 50, 550);
+        withdrawTransaction = new Transaction("18/02/2020", -50, 550);
+      });
 
     it("creates a transaction", function(){
-        expect(transaction()).toEqual(new Transaction)
+        expect(depositTransaction).toEqual(new Transaction("18/02/2020", 50, 450))
+        expect(withdrawTransaction).toEqual(new Transaction("18/02/2020", -50, 450))
     });
 
-    it("includes all fields - date, withdrawal/deposit and current balance", function(){
-        expect(transaction()).toHaveBeenCalledWith(date, deposit, withdrawal, balance)
+    it("includes the date", function(){
+        expect(depositTransaction.getDate()).toEqual("18/02/2020")
     });
+
+    it("includes deposit or withdrawal amount", function(){
+        expect(depositTransaction.getAmount()).toEqual(50)
+        expect(withdrawTransaction.getAmount()).toEqual(-50)
+    });
+
+    it("includes the balance", function(){
+        expect(depositTransaction.getBalance()).toEqual(550)
+    })
+
+    // it("includes all fields - date, withdrawal/deposit and current balance", function(){
+
+    // });
 
 });
